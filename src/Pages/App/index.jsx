df58628998a8039b1a0938fbcc5ceebf5127f40c
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-07-28 19:18:53 -0500

    4. Enrutamiento con React Router DOM

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,5 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+
 import { Home } from '../Home'
 import { MyAccount } from '../MyAccount'
 import { MyOrder } from '../MyOrder'
@@ -5,17 +7,22 @@ import { MyOrders } from '../MyOrders'
 import { NotFound } from '../NotFound'
 import { SignIn } from '../SignIn'
 
+const AppRoutes = () => {
+  return useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/*', element: <NotFound /> },
+    { path: '/sign-in', element: <SignIn /> },
+  ])
+}
+
 function App() {
   return (
-    <>
-      <h1 className="bg-red-100">Title</h1>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
