d39c5ca0ac9cbb4822f8c4f61b9a9561d6c86091
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-08-04 16:10:11 -0500

    7. Componente Card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,11 @@
+import { Card } from "../../Components/Card";
 import { Layout } from "../../Components/Layout";
 
 export function Home() {
   return (
-    <Layout>Home</Layout>
+    <Layout>
+      Home
+      <Card />
+    </Layout>
   )
 }
