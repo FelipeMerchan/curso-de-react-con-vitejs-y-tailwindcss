b4e2128120ad589044daf8cb66cedb97aa1b308f
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-08-11 15:57:57 -0500

    15. Agregando productos al carrito

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -5,7 +5,7 @@ import { useUi } from '../../hooks/useUi';
 
 /* eslint-disable react/prop-types */
 export const Card = ({ data }) => {
-  const { count, setCount, setProductToShow } = useShoppingCart();
+  const { setProductToShow, addProductsToCart } = useShoppingCart();
   const { openProductDetail } = useUi();
 
   const showProduct = (productDetail) => {
@@ -29,7 +29,7 @@ export const Card = ({ data }) => {
         />
         <button
           className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-          onClick={() => setCount(count + 1)}
+          onClick={() => addProductsToCart(data)}
         >
           <PlusIcon className='h-6 w-6 text-black' />
         </button>
--- src/Context/shoppingCart.jsx
+++ src/Context/shoppingCart.jsx
@@ -7,13 +7,22 @@ export const ShoppingCartProvider = ({ children }) => {
   const [count, setCount] = useState(0);
   /* Product detail - show product */
   const [productToShow, setProductToShow] = useState({});
+  /* Shopping Cart - Add products to cart */
+  const [cartProducts, setCartProducts] = useState([]);
+
+  const addProductsToCart = (productData) => {
+    setCartProducts([...cartProducts, productData]);
+    setCount(count + 1);
+  }
 
   return (
     <ShoppingCartContext.Provider value={{
+      cartProducts,
       count,
       productToShow,
 
       /* Methods */
+      addProductsToCart,
       setCount,
       setProductToShow,
     }}>
