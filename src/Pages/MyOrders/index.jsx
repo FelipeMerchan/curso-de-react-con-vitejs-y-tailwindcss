b1b31714f91d872f90a6adfef0e648fda307f6d2
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-08-17 01:14:25 -0500

    25. Reto: órdenes de compra con TailwindCSS

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -10,7 +10,7 @@ export function MyOrders() {
   return (
     <Layout>
       <div className='flex w-80 items-center justify-center relative mb-6'>
-        <h1>My Orders</h1>
+        <h1 className="font-medium text-xl">My Orders</h1>
       </div>
       {
         shoppingCartContext.order.map((order) => (
